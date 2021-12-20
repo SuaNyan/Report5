a922e4594574fa824ede2015116e5993e8185f39
Yuuna Takazato <e215724@ie.u-ryukyu.ac.jp>
2021-12-21 02:38:14 +0900

    例外処理の追加

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 class Main{
     public static void main(String[] args){
+        try{
             String str = null;
             System.out.println(str.length());
+
+        }catch(NullPointerException e){
+        System.out.println
+            ("NullPointException例外をcatchしました");
+        System.out.println("ーースタックトレース（ここから）ーー");
+        e.printStackTrace();
+        System.out.println("ーースタックトレース（ここまで）ーー");
+
+        }
+            
     }
 }
