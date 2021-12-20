5322d109926a2823f2c99a84b3418d9b63d60319
Yuuna Takazato <e215724@ie.u-ryukyu.ac.jp>
2021-12-21 02:54:11 +0900

    parseIntの動作確認

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 class Main{
     public static void main(String[] args){
         try{
-            String str = null;
-            System.out.println(str.length());
-
-        }catch(NullPointerException e){
-        System.out.println
-            ("NullPointException例外をcatchしました");
-        System.out.println("ーースタックトレース（ここから）ーー");
-        e.printStackTrace();
-        System.out.println("ーースタックトレース（ここまで）ーー");
-
-        }
-            
+            String str = "百二十三";
+           int i = Integer.parseInt(str);
+        }catch(NumberFormatException e){
+            System.out.println("NumberFormatException例外をcatchしました");
+        }        
     }
 }
